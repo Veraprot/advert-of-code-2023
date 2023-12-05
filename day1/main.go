fea2602f418738cc90937993d88aed8ccb71b447
Vera Protopopova <vera.protopopova@gmail.com>
2023-12-04 22:20:49 -0500

    finish challenge

--- day1/main.go
+++ day1/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -14,18 +14,25 @@ func main() {
 	countCalibrationValues(calibrationLines)
 }
 
-func countCalibrationValues(s []string) {
+func countCalibrationValues(s []string) int {
+	result := 0
 	for _, calibrationLine := range s {
-		fmt.Println(calibrationLine)
-		GetCalibrationValue(calibrationLine)
+		lineValue := GetCalibrationValue(calibrationLine)
+
+		i, err := strconv.Atoi(lineValue)
+		if err != nil {
+			panic(err)
+		}
+
+		result += i
 	}
+
+	return result
 }
 
 func GetCalibrationValue(s string) string {
 	re := regexp.MustCompile("[0-9]")
 	numValues := re.FindAllString(s, -1)
-
-	fmt.Println(numValues)
 
 	if len(numValues) == 0 {
 		return "0"
--- day1/main_test.go
+++ day1/main_test.go
@@ -17,3 +17,14 @@ func TestGetCalibrationValue(t *testing.T) {
 	test("treb7uchet", "77")
 	test("trebuchet", "0")
 }
+
+func TestCountCalibrationValues(t *testing.T) {
+	test := func(input []string, result int) {
+		assert.Equal(t, result, countCalibrationValues(input))
+	}
+	// ill assume that all digits can be only positive
+	test([]string{}, 0)
+	test([]string{"1abc2", "pqr3stu8vwx"}, 50)
+	test([]string{"pqr3stu8vwx"}, 38)
+	test([]string{"treb7uchet", "0"}, 77)
+}
