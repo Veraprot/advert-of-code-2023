34223feee55b9b03acb0260174930b615e698e9a
Vera Protopopova <vera.protopopova@gmail.com>
2023-12-06 17:01:35 -0500

    fix tests

--- day1/main_test.go
+++ day1/main_test.go
@@ -6,18 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetCalibrationValue(t *testing.T) {
-	test := func(input, result string) {
-		assert.Equal(t, result, GetCalibrationValue(input))
-	}
-	// ill assume that all digits can be only positive
-	test("1abc2", "12")
-	test("pqr3stu8vwx", "38")
-	test("a1b2c3d4e5f", "15")
-	test("treb7uchet", "77")
-	test("trebuchet", "0")
-}
-
 func TestCountCalibrationValues(t *testing.T) {
 	test := func(input []string, result int) {
 		assert.Equal(t, result, countCalibrationValues(input))
